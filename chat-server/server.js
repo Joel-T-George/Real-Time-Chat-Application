4f232b366a74f2e516177503ecbc22e046a9ef73
Joel-T-George <joelthomas0203@gmail.com>
2023-04-10 12:31:53 +0530

    test

--- chat-server/server.js
+++ chat-server/server.js
@@ -4,5 +4,7 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Chat Server activiated')
 })
-
+app.get('/', function (req, res) {
+    res.send('Chat Server activiated')
+})
 app.listen(9000)
