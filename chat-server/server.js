42ab7c4b150b45cdc2c6621f0135c358c4ed2053
Joel-T-George <joelthomas0203@gmail.com>
2023-05-11 23:52:39 +0530

    update correction

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 uri = process.env.DATABASE_URL
 const app = express()
