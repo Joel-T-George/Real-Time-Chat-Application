099178173624d30bfc465fd8177006c0d44aa0ce
Joel-T-George <joelthomas0203@gmail.com>
2023-05-13 20:09:21 +0530

    Link active

--- chat-app/pages/Welcome.tsx
+++ chat-app/pages/Welcome.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Welcome: React.FC<{}> = ()=>{
   return (
     <div className="Login" >
-      "Welcome"
+        <Link to="/login">Login</Link>
+        <br></br>
+        <Link to= "/SignUp">Register</Link>
     </div>
   )
 }
