41834ed5ca30b7196ea75ac96ce42f8dfc974551
Joel-T-George <joelthomas0203@gmail.com>
2023-05-14 00:24:43 +0530

    heading to the register form

--- chat-app/pages/Register.tsx
+++ chat-app/pages/Register.tsx
@@ -3,7 +3,7 @@ import RegisterHandler from './../components/Register'
 const Register: React.FC<{}> = ()=>{
   return (
     <>
-      
+      <h1 className="text-3xl font-bold ">Create account</h1>
       <RegisterHandler/>
 
     </>
