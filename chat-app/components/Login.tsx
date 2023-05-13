7f2aa400a95ee16582435fb6e7fb2a87ce41ed04
Joel-T-George <joelthomas0203@gmail.com>
2023-05-13 23:51:19 +0530

    login form updated

--- chat-app/components/Login.tsx
+++ chat-app/components/Login.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState }  from "react";
 
 const Loginhandler: React.FC<{}> = ()=>{
+  const [email,setEmail] = useState()
+  const [username,setUsername]=useState()
+  const [password,setPassword]=useState()
+
+  const submitHandler = (e:any)=>{
+    e.preventDefault();
+
+    console.log(email,username,password)
+
+  }
   return (
-    <div>
-      "Dashboard"
-    </div>
+
+    <form onSubmit={submitHandler} >
+      <label htmlFor="username">Username</label>
+      <input  value ={username} onChange={(e:any)=>{ setUsername(e.target.value)}} type="text" placeholder="username" id="username" />
+      <label htmlFor="email">Email</label>
+      <input value ={email} onChange={(e:any) =>{ setEmail(e.target.value)}}  type="email" placeholder="your@email.com" id="email" />
+      <label htmlFor="password">Password</label>
+      <input value ={password} onChange={(e:any) =>{ setPassword(e.target.value)}} type="password" placeholder="your password" id="password" />
+      <button type="submit">Log In</button>
+    </form>
   )
 }
 
--- chat-app/pages/Login.tsx
+++ chat-app/pages/Login.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Loginhandler from './../components/Login'
 const Login: React.FC<{}> = ()=>{
   return (
     <div className="Login" >
-      
+      <Loginhandler/>
       <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
