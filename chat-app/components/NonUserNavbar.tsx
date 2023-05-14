4ab5e85b6382cdd406f121afcce1122c87f2a062
Joel-T-George <joelthomas0203@gmail.com>
2023-05-15 00:13:56 +0530

    responsive nave bar

--- chat-app/components/NonUserNavbar.tsx
+++ chat-app/components/NonUserNavbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 const Navigation = [
   ['Home', '/welcome/'],
@@ -7,10 +7,14 @@ const Navigation = [
   ['Contact', '/welcome/Contact']
 ]
 const NonNavbar: React.FC<{}> = ()=>{
+  const [open,setOpen]=useState()
   return (
-    <div className="shadow-lg md:w-full w-2/3 h-full md:h-auto fixed top-0 md:m-0 left-0 bg-sky-800/90  md:p-4 " >
-      <nav className="flex  md:justify-end space-x-4 md:m-0 ">
-        <ul className="md:inline-flex "> {
+    <div className="shadow-lg md:w-full w-full h-2/4 md:h-auto fixed md:flex items-center justify-around top-0 md:m-0 left-0 bg-sky-800/90  md:p-4 " >
+      
+      <nav className="flex  md:justify-start space-x-4 md:m-0 text-white md:text-lg text-lg font-medium ">
+      <span className=" md:m-0 text-3xl md:pt-1 m-1 mr-2"  >Real Chat</span>
+        <span className="text-2xl absolute right-5 top-3 md:hidden " >X</span>
+        <ul className="md:inline-flex absolute md:static md:m-0 mt-5 md:z-auto z-[-1] pl-3 transition-all duration-500 ease-in"> {
         Navigation.map(([title, url]) => (
           <li key={title} className="text-white   md:text-lg text-lg font-medium md:m-2 m-5">
             <NavLink
