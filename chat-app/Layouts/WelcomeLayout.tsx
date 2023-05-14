4a4fc4e068aca1cfe731f2a4a959e6f776c23e46
Joel-T-George <joelthomas0203@gmail.com>
2023-05-14 22:56:15 +0530

    error -4

--- chat-app/Layouts/WelcomeLayout.tsx
+++ chat-app/Layouts/WelcomeLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from './../components/NonUserNavbar'
 import WelcomeLayoutRoute from "./../Routelayout/WelcomeRouteLayout"
 
-function WelcomeLayout() {
+const WelcomeLayout:React.FC<{}> = ()=> {
   return (
     <>
       <Navbar />
